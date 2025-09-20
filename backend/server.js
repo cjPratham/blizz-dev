635b4e89728d00f1e8d9b55ed0518aeeb47c3c34
cjPratham <pratham007yadav@gmail.com>
2025-09-20 20:46:20 +0530

    added student endpoint in the server.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 
 app.use("/api/auth",auth);
 app.use("/api/teacher",require("./src/routes/teacherRoutes"));
+app.use("/api/student",require("./src/routes/studentRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
