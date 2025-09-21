4bfef89378d5c720ffc6b24adffb455ddf9c8591
cjPratham <pratham007yadav@gmail.com>
2025-09-21 11:06:14 +0530

    frontend folder structure ready

---
+++ frontend/src/api/teacher.js
@@ -0,0 +1,6 @@
+// dummy usage of api
+
+// import api from "./axios";
+
+// export const createClass = (data) => api.post("/teacher/classes", data);
+// export const createSession = (data) => api.post("/teacher/sessions", data);
---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  define: {
+    __API_URL__: JSON.stringify(process.env.VITE_API_URL)
+  }
+})
