8aee839834c56c980010bed32ab8116e6d91930e
cjPratham <pratham007yadav@gmail.com>
2025-09-26 21:26:39 +0530

    checking the frontend time

--- frontend/src/pages/teacher/Sessions.jsx
+++ frontend/src/pages/teacher/Sessions.jsx
@@ -113,7 +113,8 @@ const Sessions = () => {
           >
             <div className="flex-1">
               <p className="font-semibold">
-                Start: {new Date(s.startTime).toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })}
+                {/* Start: {new Date(s.startTime).toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })} */}
+                Start: {s.startTime}
               </p>
               <p>End: {new Date(s.endTime).toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })}</p>
               <p>
