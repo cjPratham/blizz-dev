168fcb9ce5d1f1ad121291ed0af44aff82592706
cjPratham <pratham007yadav@gmail.com>
2025-09-26 17:41:43 +0530

    deployed backend

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL;
+const API = __API_URL__;
 
 export const signup = (data) => axios.post(`${API}/auth/register`, data);
 export const login = (data) => axios.post(`${API}/auth/login`, data);
