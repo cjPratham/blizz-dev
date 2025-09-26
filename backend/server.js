480c19e751d54c64edb2bdc6a80e119b4e5521b4
cjPratham <pratham007yadav@gmail.com>
2025-09-27 00:08:04 +0530

    updated CORS

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://blizz-attendance.vercel.app/"], // Chrome requires exact origin
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth",auth);
